6d880e20a07b92fb34057c8f2e4096c5db75f8a8
brice <brice.beumier@hotmail.fr>
2022-09-15 18:55:03 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const voitures = ["Renault Scénic", "Fiat 500", "Ford Escort"];
+console.log(voitures[0]);
+console.log(voitures[1]);
+console.log(voitures[2]);
+console.log(voitures.length);
+for (let cptI = 0; cptI < voitures.length; cptI++) {
+console.log(voitures[cptI]);
+}
